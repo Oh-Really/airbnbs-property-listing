c339e1aa1de6de04b5d16132f1dca996ef2d2c0c
Oh-Really <moreilly406@gmail.com>
2023-09-24 22:39:19 +0100

    Updated tabular_data.py to clean original df, and added function to return features and labels

--- tabular_data.py
+++ tabular_data.py
@@ -3,14 +3,75 @@
 import os
 from pathlib import Path
 import pandas as pd
+import ast
+import numpy as np
 
-root_dir = os.getcwd()
-csv_path = Path(root_dir, 'airbnb-property-listings', 'tabular_data', 'listing.csv')
-if csv_path.is_file():
-    listings_df = pd.read_csv(csv_path)
-else:
-    print(f'File {csv_path} does not exist')
 
+def load_data():
+    root_dir = os.getcwd()
+    csv_path = Path(root_dir, 'airbnb-property-listings', 'tabular_data', 'listing.csv')
+    if csv_path.is_file():
+        listings_df = pd.read_csv(csv_path)
+    else:
+        print(f'File {csv_path} does not exist')
+    return listings_df
 
 def remove_rows_with_missing_ratings(df: pd.DataFrame) -> pd.DataFrame:
-    return df.dropna(subset=['Cleanliness_rating', 'Accuracy_rating', 'Communication_rating', 'Location_rating', 'Check-in_rating', 'Value_rating'], inplace=True)
+    return df.dropna(subset=['Cleanliness_rating', 'Accuracy_rating', 'Communication_rating', 'Location_rating', 'Check-in_rating', 'Value_rating'])
+
+def remove_whitespace(list):
+    for string in list:
+        #Checks to see if string is empty
+        if not string.strip():
+            try:
+                list.remove(string)
+            except:
+                pass
+    return list
+
+def combine_description_strings(df: pd.DataFrame) -> pd.DataFrame:
+    '''Cleans up Description column of the dataframe'''
+    null_desc = df['Description'].isna()
+    df = df[~null_desc]
+    df.loc[:, 'Description'] = df['Description'].apply(convert_description_strings)
+    return df
+
+def convert_description_strings(desc):
+    '''Converts description values to list, removes first item and any whitespace, and concatenates the string'''
+    try:
+        desc = ast.literal_eval(desc) # convert to list
+    except SyntaxError:
+        print('Skipping conversion to string; already a string')
+    try:
+        desc.remove(desc[0])
+    except AttributeError:
+        pass
+
+    desc = remove_whitespace(desc)
+    desc = ' '.join(desc)
+    return desc
+
+def set_default_feature_values(df):
+    return df.fillna(value={'guests': 1, 'beds': 1, 'bathrooms': 1, 'bedrooms': 1})
+
+
+def clean_tabular_data(df: pd.DataFrame) -> pd.DataFrame:
+    df = remove_rows_with_missing_ratings(df)
+    df = combine_description_strings(df)
+    df = set_default_feature_values(df)
+    return df
+
+
+def load_airbnb(df, label : str):
+    features = df.select_dtypes(include=[np.number])
+    labels = features.pop(label)
+    return features, labels
+
+
+
+if __name__ == "__main__":
+    df = load_data()
+    df = clean_tabular_data(df)
+    output_filepath = Path(os.getcwd(), 'airbnb-property-listings', 'tabular_data', 'clean_tabular_data.csv')
+    df.to_csv(output_filepath)
+# %%
